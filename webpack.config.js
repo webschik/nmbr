6f907ffd8a61eae945bbda09889ea1965a92e59c
Denys Kniazevych <webschik@gmail.com>
2017-11-08 17:03:36 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     ],
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.css'],
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.css']
     },
 
     module: {
